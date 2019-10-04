ac9243ab8d5ba4007a7661ffaf9bb3202a96c74f
Deniz <deniz.scholz@t-online.de>
2019-10-04 17:22:39 +0200

    started design

--- website/webapp/views.py
+++ website/webapp/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.views import View
 
 # Create your views here.
+
+
+class Home(View):
+    def get(self, request):
+        context = {}
+        return render(request, 'webapp/index.html', context=context)
